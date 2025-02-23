a3009a5c5706b8d93cbfba9578aa6748914af492
Nuhan Alam <nuhanalam@gmail.com>
2025-02-23 16:28:00 +0600

    Unchecked Bellman_Ford Algo

--- Bellman_Ford.cpp
+++ Bellman_Ford.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-//Took input as reference
+//Input with reference and array to increase porficiency
 class Edge
 {
     public:
@@ -23,7 +23,9 @@ int main()
     
     int v,e;
     cin >> v >> e;
-    
+    int ans[v];
+    fill(&ans[0],&ans[0]+v,INT_MAX);
+    ans[0]=0;
     Edge* edge_list[v];
     for(int i=0;i<v;i++)
     {
@@ -33,9 +35,41 @@ int main()
         edge_list[i]=a;
         
     }
-    for(auto ed: edge_list)
+    // for(auto ed: edge_list)
+    // {
+    //     cout << ed->from << " " <<  ed->to  << " " << ed->weight << endl;
+    // }
+    bool negative_cycle = false;
+    for(int i=v;i>=0;i--)
     {
-        cout << ed->from << " " <<  ed->to  << " " << ed->weight << endl;
+        //cout << "In outer Looop" << endl;
+        for(auto eg:edge_list)
+        {
+            //cout << "In inner Looop" << endl;
+            if(ans[eg->from]!=INT_MAX && ans[eg->from]+eg->weight < ans[eg->to])
+            {
+                ans[eg->to]=ans[eg->from]+eg->weight;
+                //cout << ans[eg->to]<< endl;
+            }
+        }
+    }
+
+    for(auto eg:edge_list)
+        {
+            if(ans[eg->from]+eg->weight<ans[eg->to])
+            {
+                negative_cycle = true;
+                break;
+            }
+        }
+
+    if(negative_cycle) cout << "Negative Cycle Ditectied" << endl;
+    else
+    {
+        for(int i=0;i<v;i++)
+        {
+            cout << i << " -> " << ans[i]<< endl;
+        }
     }
 return 0;
 
