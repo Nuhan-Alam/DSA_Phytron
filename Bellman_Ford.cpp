617d6f368d0c711fbeb811a19bb08512faefe355
Nuhan Alam <nuhanalam@gmail.com>
2025-02-23 15:35:35 +0600

    Input with reference

---
+++ Bellman_Ford.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Edge
+{
+    public:
+    int from;
+    int to;
+    int weight;
+
+    Edge(int from, int to, int weight)
+    {
+        this->from = from;
+        this->to = to;
+        this->weight = weight;
+    }
+    
+};
+
+
+int main()
+{
+    
+    int v,e;
+    cin >> v >> e;
+    
+    vector<Edge*> edge_list(v);
+    for(int i=0;i<v;i++)
+    {
+        int from,to,weight;
+        cin >> from >> to >> weight;
+        Edge* a = new Edge(from,to,weight);
+        edge_list[i]=a;
+        
+    }
+    for(auto ed: edge_list)
+    {
+        cout << ed->from << " " <<  ed->to  << " " << ed->weight << endl;
+    }
+return 0;
+
+}
